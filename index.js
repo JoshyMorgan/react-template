b09277f68927fd55499413b3892340862352f0e2
JoshyMorgan <41779988+JoshyMorgan@users.noreply.github.com>
2020-03-06 19:17:10 +1100

    import updated in index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import express from "express"
 var express = require("express")
 var app = express()
 var path = require("path")
@@ -6,6 +7,7 @@ var path = require("path")
 app.use(express.static(path.join(__dirname)))
 app.use("/images", express.static(__dirname + "/images"))
 app.use("components", express.static(__dirname + "/components"))
+app.use("/scripts", express.static(__dirname));
 app.use("/semantic", express.static(__dirname + "/semantic"))
 
 //
