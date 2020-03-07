8b32cbc723f98241404fcf0ac4a8516613e2d8a4
JoshyMorgan <41779988+JoshyMorgan@users.noreply.github.com>
2020-03-08 00:49:48 +1100

    testing 1.0

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import express, { static } from 'express';
+var express = require('express');
 var app = express();
-import { join } from 'path';
+var path = require('path');
 
 
 //locate the directory 
-app.use(static(join(__dirname)))
-app.use("/images", static(__dirname + "/images"))
-app.use("/components", static(__dirname + "/components"))
-app.use("/scripts", static(__dirname));
-app.use("/semantic", static(__dirname + "/semantic"))
+app.use(express.static(path.join(__dirname)))
+app.use("/images", express.static(__dirname + "/images"))
+app.use("/components", express.static(__dirname + "/components"))
+app.use("/scripts", express.static(__dirname));
+app.use("/semantic", express.static(__dirname + "/semantic"))
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
