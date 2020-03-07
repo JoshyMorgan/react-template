0f7f6df32d11ff6e4ecdafd652a3db38e52984f4
JoshyMorgan <41779988+JoshyMorgan@users.noreply.github.com>
2020-03-08 00:06:43 +1100

    implement requirejs 1.0

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-// var express = require('express');
-// var app = express();
-// var path = require('path');
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+var express = requirejs('express');
+var app = express();
+var path = requirejs('path');
 
 
-// //locate the directory 
-// app.use(express.static(path.join(__dirname)))
-// app.use("/images", express.static(__dirname + "/images"))
-// app.use("/components", express.static(__dirname + "/components"))
-// app.use("/scripts", express.static(__dirname));
-// app.use("/semantic", express.static(__dirname + "/semantic"))
+//locate the directory 
+app.use(express.static(path.join(__dirname)))
+app.use("/images", express.static(__dirname + "/images"))
+app.use("/components", express.static(__dirname + "/components"))
+app.use("/scripts", express.static(__dirname));
+app.use("/semantic", express.static(__dirname + "/semantic"))
 
-// // viewed at based directory http://localhost:8080/
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/index.html'));
-// });
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
