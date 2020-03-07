c50d97ad925ea15776e5c8fcdb48cc4d6c1b9f34
JoshyMorgan <41779988+JoshyMorgan@users.noreply.github.com>
2020-03-07 22:31:39 +1100

    index.js ES6 updated

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express');
+import express, { static } from 'express';
 var app = express();
-var path = require('path');
+import { join } from 'path';
 
 
 //locate the directory 
-app.use(express.static(path.join(__dirname)))
-app.use("/images", express.static(__dirname + "/images"))
-app.use("/components", express.static(__dirname + "/components"))
-app.use("/scripts", express.static(__dirname));
-app.use("/semantic", express.static(__dirname + "/semantic"))
+app.use(static(join(__dirname)))
+app.use("/images", static(__dirname + "/images"))
+app.use("/components", static(__dirname + "/components"))
+app.use("/scripts", static(__dirname));
+app.use("/semantic", static(__dirname + "/semantic"))
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(join(__dirname + '/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
