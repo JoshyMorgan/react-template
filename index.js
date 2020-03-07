27c5cc13b5aa4477135a3f99cfa3ab111742d8a7
JoshyMorgan <41779988+JoshyMorgan@users.noreply.github.com>
2020-03-07 22:15:10 +1100

    restore version 1.0

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/components", express.static(__dirname + '/components'));
-app.use("/semantic", express.static(__dirname + '/semantic'));
+
+//locate the directory 
+app.use(express.static(path.join(__dirname)))
+app.use("/images", express.static(__dirname + "/images"))
+app.use("components", express.static(__dirname + "/components"))
+app.use("/scripts", express.static(__dirname));
+app.use("/semantic", express.static(__dirname + "/semantic"))
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
