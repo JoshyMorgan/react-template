0bb91ca5a49dfb23674b6f86377262b51cd792ef
JoshyMorgan <41779988+JoshyMorgan@users.noreply.github.com>
2020-03-08 00:01:00 +1100

    implement index.js directly on index.html

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+// var express = require('express');
+// var app = express();
+// var path = require('path');
 
 
-//locate the directory 
-app.use(express.static(path.join(__dirname)))
-app.use("/images", express.static(__dirname + "/images"))
-app.use("/components", express.static(__dirname + "/components"))
-app.use("/scripts", express.static(__dirname));
-app.use("/semantic", express.static(__dirname + "/semantic"))
+// //locate the directory 
+// app.use(express.static(path.join(__dirname)))
+// app.use("/images", express.static(__dirname + "/images"))
+// app.use("/components", express.static(__dirname + "/components"))
+// app.use("/scripts", express.static(__dirname));
+// app.use("/semantic", express.static(__dirname + "/semantic"))
 
-// viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+// // viewed at based directory http://localhost:8080/
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
